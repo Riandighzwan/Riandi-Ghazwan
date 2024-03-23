293bb7e7eb42e4ecf88c47c1bbeb5ddd430551f9
Riandighzwan <164618845+Riandighzwan@users.noreply.github.com>
2024-03-23 16:13:56 +0700

    Add files via upload

---
+++ ukk/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Galeri Foto</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<style>
+  .banner{
+    height:100vh;
+    background: url('assets/img/bg1.jpg');
+    background-size:cover;
+    background-position:center;
+  }
+  </style>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">My Photo Gallery</a>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+        
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+
+
+
+<div class="container-fluif banner">
+</div>
+    
+<script type="text/javascript" src="assets/css/bootstrap.min.js"></script>
+</body>
+</html>
